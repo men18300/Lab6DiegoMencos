731486fa30087942742fb2cf465d291dd6129d41
men18300 <77988142+men18300@users.noreply.github.com>
2021-05-10 00:29:43 -0600

    Lab Terminado
    Ya esta comentado y todo

--- main.c
+++ main.c
@@ -14,62 +14,104 @@
 #include"driverlib/sysctl.h"
 #include"driverlib/gpio.h"
 
+//Variables que utilizare a lo largo del programa
+int dlay=0;
 int status=1;
 int status2=1;
+int LED=2;
+
+//Prototipo de funciones
+void delaylab (void);
+void delayMs(uint32_t ui32Ms);
 
 int main(void) {
-    //Establecer reloj del microcontrolador
-    SysCtlClockSet(SYSCTL_XTAL_16MHZ|SYSCTL_SYSDIV_5);
+    //Establecer reloj del microcontrolador a 50 MGHZ
+    SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
     //Habilitar periférico GPIO F
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
     //Habilitar pines de salida y entrada
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
-    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_0|GPIO_PIN_4);
+   // GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_0|GPIO_PIN_4);
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_0);
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4);
     //Configurar tipo de entrada
-    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_0|GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+
+    //Configura los pushbuttoms con weak pull-up
+   // GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_0|GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_0, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
 
     while(true){
         //Lee el pin de entrada
-     //   status = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0);
         status2 = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4);
 
 
-        //Si el botón está presionado enciende el LED y si no, lo apaga.
-        if (status==0){
-            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 2);
-            SysCtlDelay(1000000);
-            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 10);
-            SysCtlDelay(1000000);
-            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 8);
-            SysCtlDelay(8000000);
+        //Parte 3
+        if (status==1){
+            status=0;
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, LED); //Rojo
+            delaylab();
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, LED | 0b1000 );//Amarillo
+            delaylab();
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, LED<<2);//Verde
+            delaylab();
             GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0);
-            SysCtlDelay(8000000);
-            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 8);
-            SysCtlDelay(8000000);
+            delaylab();
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, LED<<2);
+            delaylab();
             GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0);
-            SysCtlDelay(8000000);
+            delaylab();
 
         }
 
+        //Parte 4
         else if (status2==0){
-
-            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 8);
-            SysCtlDelay(10000000);
+            delayMs(500);
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 8);//Verde
+            delayMs(1000);
             GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0);
-            SysCtlDelay(10000000);
+            delayMs(500);
             GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 8);
-            SysCtlDelay(8000000);
+            delayMs(500);
             GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0);
-            SysCtlDelay(8000000);
-            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 10);
-            SysCtlDelay(8000000);
-            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 2);
-            SysCtlDelay(8000000);
+            delayMs(500);
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 8);
+            delayMs(500);
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 10);//Amarillo
+            delayMs(1000);
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 2);//Rojo
+            delayMs(1000);
 
         }
 
+        //
         else{
+
+            //Se apagan todos los leds
             GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0);
         }
     }
+}
+
+
+//Este delay sirve para poder utilizar el delay que traen las librerias pero con un
+//valor exacto en ms
+void delayMs(uint32_t ui32Ms) {
+
+    // 1 clock cycle = 1 / SysCtlClockGet() second
+    // 1 SysCtlDelay = 3 clock cycle = 3 / SysCtlClockGet() second
+    // 1 second = SysCtlClockGet() / 3
+    // 0.001 second = 1 ms = SysCtlClockGet() / 3 / 1000
+
+    SysCtlDelay(ui32Ms * (SysCtlClockGet() / 3 / 1000));
+}
+
+//Delay programado para la parte 3
+void delaylab (void){
+    dlay=0;
+while (dlay!=2000000){
+    dlay++;
+
+}
+
 }
